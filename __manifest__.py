46a186f80d9b6c8e41d35858b9642ddc8b65f510
AbogeJr <junioraboge@gmail.com>
2023-06-26 23:16:36 +0300

    initialize

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS Custom Screen",
+    "version": "1.0",
+    "depends": ["point_of_sale", "sale_management"],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_custom_sales_order/static/src/xml/sale_order_button.xml",
+            "pos_custom_sales_order/static/src/js/sale_order_button.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ static/src/js/sale_order_button.js
@@ -0,0 +1,71 @@
+odoo.define("pos_custom_screen.CategoryControlButton", function (require) {
+  "use strict";
+  const { Gui } = require("point_of_sale.Gui");
+  const PosComponent = require("point_of_sale.PosComponent");
+  const ProductScreen = require("point_of_sale.ProductScreen");
+  const { useListener } = require("@web/core/utils/hooks");
+  const Registries = require("point_of_sale.Registries");
+  class CreateSaleOrder extends PosComponent {
+    setup() {
+      super.setup();
+      useListener("click", this.onClick);
+    }
+
+    async onClick() {
+      //            for creating sale order from pos throgh a button
+      console.log("onclick entered");
+      const order = this.env.pos.get_order();
+      var cus = order.get_partner();
+      var lines = order.get_orderlines();
+      if (lines.length === 0) {
+        Gui.showPopup("ErrorPopup", {
+          title: this.env("Please select a product"),
+          body: this.env(
+            "You cannot create sales order without selecting a product."
+          ),
+        });
+        return;
+      } else if (!cus) {
+        Gui.showPopup("ErrorPopup", {
+          title: this.env("Please select a customer"),
+          body: this.env(
+            "You cannot create sales order without selecting a customer."
+          ),
+        });
+        return;
+      } else {
+        const { confirmed } = await this.showPopup("ConfirmPopup", {
+          title: "Confirmation",
+          body: "Are you sure you want to create sale order?",
+        });
+        if (confirmed) {
+          var temp_list = [];
+          for (var i = 0; i < lines.length; i++) {
+            const dict = {
+              amount_undiscounted: lines[i].quantity,
+              amount_untaxed: lines[i].get_display_price(),
+              tax_totals: lines[i].get_discount(),
+              partner_id: lines[i].product.id,
+            };
+            temp_list.push(dict);
+          }
+          this.env.services.rpc({
+            model: "sale.order",
+            method: "create",
+            args: [temp_list],
+          });
+        }
+      }
+    }
+  }
+  CreateSaleOrder.template = "PosSaleOrderButton";
+
+  ProductScreen.addControlButton({
+    component: CreateSaleOrder,
+    condition: function () {
+      return true;
+    },
+  });
+  Registries.Component.add(CreateSaleOrder);
+  return CreateSaleOrder;
+});
